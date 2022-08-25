c7f34bde8cdca8bdf8c4fe02d5a8df9deb932216
Kurdeus <111803918+Kurdeus@users.noreply.github.com>
2022-08-25 17:40:41 +0430

    Delete getListFile.py

--- getListFile.py
+++
@@ -1,9 +0,0 @@
-from service import drive_service
-
-
-def getList(folder_id):
-    result = drive_service().files().list(supportsTeamDrives=True,
-    includeTeamDriveItems=True,q=f"'{folder_id}' in parents and trashed = false",
-    spaces='drive',pageSize=1000,fields='files(id, name, mimeType, size, shortcutDetails)',
-    orderBy='folder, name').execute()
-    return [c for c in [x for x in [result[i] for i in result][0]]]
